2d709037d4ef128d0af3fcacb2aaf2117096a30b
logg_ <csw1392153909@163.com>
2022-07-04 23:52:14 +0800

    style: 修改声明

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './app.scss'
+import Img from './assets/avatar.jpg'
 
 function App() {
   return (
     <>
       <h2>react-p</h2>
-      <img src="./assets/avatar.jpg" />
+      <img src={Img} />
     </>
   );
 }
